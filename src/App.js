cb1a954adf3afc10ca914321dbd934f504a88f5a
mac <chukwuekeuzoma@gmail.com>
2022-11-24 14:38:42 +0100

    updatd the component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import ClassOneComponent from './Components/ClassOneComponent'
+import logo from "./logo.svg";
+import "./App.css";
+import ConstructorMethodsinClassComponent from "./Components/ConstructorMethodsinClassComponent";
+import ClassOneComponent from "./Components/ClassOneComponent";
+import StateInClassComponent from "./Components/StateInClassComponent";
 
 function App() {
   return (
     <>
-      <ClassOneComponent/>
+      {/* <ClassOneComponent /> */}
+      {/* <StateInClassComponent /> */}
+      <ConstructorMethodsinClassComponent />
     </>
   );
 }
